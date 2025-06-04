6bbd0a5ef027b6e0e0606c885df4aeb895228fac
Leonardo <felixleonardosabysuarez16@gmail.com>
2025-06-03 20:00:46 -0400

    formulario base completado con error en la clase

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,11 @@ class MyApp extends StatelessWidget {
         ),
           useMaterial3:true
       ),
+      home: RegistrationFromScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class RegistrationFromScreen {
 }
