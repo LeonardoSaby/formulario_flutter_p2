b24f991ef04cf0eeaf4767df35b556e42484ab68
Leonardo <felixleonardosabysuarez16@gmail.com>
2025-06-04 21:10:02 -0400

    codigo ya corregido de la prueba en clases

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -9,7 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     //throw UnimplementedError();
     return MaterialApp(
       title: "Formulario de Registro",
@@ -28,5 +29,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RegistrationFromScreen {
+class RegistrationFromScreen  extends StatelessWidget {
+  static const Color primaryNavy = Color(0xFF1E3A8A);
+  static const Color backgroundGray = Color(0xFFF9FAFB);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius:
+      BorderRadius.circular(12),
+      boxShadow:[
+        BoxShadow(
+          color:
+          backgroundGray.withOpacity(0.1),
+          blurRadius: 10,
+          offset: Offset(0, 4),
+        )
+      ],
+      ),
+      child: Column(
+      children: [
+        Icon(Icons.person_add, size: 48, color: primaryNavy),
+        SizedBox(height: 12),
+      Text(
+      "Datos personales",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: primaryNavy
+      )
+      ),
+      SizedBox(height: 8),
+        Text(
+          "Complete todos los campos requeridos",
+          style: TextStyle(fontSize: 14, color: backgroundGray),
+        ),
+        ],
+        ),
+        );
+  }
+
 }
